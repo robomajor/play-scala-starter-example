5f6dd35e2b2b4432237314801658374eb5100e4a
robomajor <Robota40%>
2022-05-11 22:37:47 +0200

    websocket chat app

---
+++ app/controllers/WebSocketChat.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import actors.{ChatActor, ChatManager}
+import akka.actor.{ActorSystem, Props}
+import akka.stream.Materializer
+
+import javax.inject._
+import play.api.mvc._
+import play.api.libs.streams.ActorFlow
+
+@Singleton
+class WebSocketChat @Inject() (cc: ControllerComponents) (implicit assetsFinder: AssetsFinder, system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+
+  val manager = system.actorOf(Props[ChatManager], "Manager")
+
+  def index = Action { implicit request =>
+    Ok(views.html.chatPage())
+  }
+
+  def socket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef { out =>
+      ChatActor.props(out, manager)
+    }
+  }
+}
